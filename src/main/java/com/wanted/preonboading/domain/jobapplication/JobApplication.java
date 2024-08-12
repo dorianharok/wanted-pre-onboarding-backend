e3a46d43d7e94727697124d84affd63c98fad66f
harok <dorianharok@github.com>
2024-08-12 22:42:17 +0900

    fix: JobApplication 연관관계 수정

--- src/main/java/com/wanted/preonboading/domain/jobapplication/JobApplication.java
+++ src/main/java/com/wanted/preonboading/domain/jobapplication/JobApplication.java
@@ -1,9 +1,10 @@
 package com.wanted.preonboading.domain.jobapplication;
 
-import com.wanted.preonboading.domain.company.Company;
+import com.wanted.preonboading.domain.jobposting.JobPosting;
 import com.wanted.preonboading.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +17,20 @@ public class JobApplication {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
-    private Company company;
+    private JobPosting jobPosting;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    public static JobApplication of(JobPosting jobPosting, User user) {
+        return JobApplication.builder()
+                .jobPosting(jobPosting)
+                .user(user)
+                .build();
+    }
+
+    @Builder
+    private JobApplication(JobPosting jobPosting, User user) {
+        this.jobPosting = jobPosting;
+        this.user = user;
+    }
 }
