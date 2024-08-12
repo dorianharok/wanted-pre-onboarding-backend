e50665507e3b0a89439e25c200616705383ae48c
harok <dorianharok@github.com>
2024-08-12 17:36:10 +0900

    first commit

---
+++ src/main/java/com/wanted/preonboading/PreOnboadingApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnboadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreOnboadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted/preonboading/PreOnboadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnboadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
